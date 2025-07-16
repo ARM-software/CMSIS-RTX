cb053ee954174c1dd8be707f79a7c0c2505521c6
Nicola Mazzucato <nicola.mazzucato@arm.com>
2025-07-16 09:53:23 +0100

    Revert "rtx_thread: Exit early if Idle thread create fails (#60)"
    Revert PR no.60 for the following reasons:
     - there haven't been test cases to test that path
     - there was in the past such check, but then removed
     - the project is working towards 100% code coverage thus, for the
       above, it does not help in that direction
    
    See https://github.com/ARM-software/CMSIS-RTX/pull/60 for further
    details.
    
    This reverts commit b1c75fc3114b85bda23e5207a877b31fe5a74360.
    
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

--- Source/rtx_thread.c
+++ Source/rtx_thread.c
@@ -2409,9 +2409,6 @@ bool_t osRtxThreadStartup (void) {
   osRtxInfo.thread.idle = osRtxThreadId(
     svcRtxThreadNew(osRtxIdleThread, NULL, osRtxConfig.idle_thread_attr)
   );
-  if (osRtxInfo.thread.idle == NULL) {
-    return FALSE;
-  }
 
   // Create Timer Thread
   if (osRtxConfig.timer_setup != NULL) {
